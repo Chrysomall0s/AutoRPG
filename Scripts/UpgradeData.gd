10bfcac172cbb9235592cbb01f500e081e33c792
Chrysomall0s <112194620+Chrysomall0s@users.noreply.github.com>
2026-06-12 17:42:18 +0200

    check

--- Scripts/UpgradeData.gd
+++ Scripts/UpgradeData.gd
@@ -11,12 +11,12 @@ var upgrades = [
 	{"name": "up4",      "category": "passive", "type": "heal",   "value": 0, "weight": 20,  "cost": 1, "icon": "res://Assets/atlas/flower.tres","index": 3,    "level": 1},
 	
 	# --- BASE WEAPONS (Equipped into any empty slot or replaces old weapon) ---
-	{"name": "Club","friendly":false, "category": "weapon", "type": "damage","scale": "hp","weight": 20, "cost": 2, "icon":           "res://Assets/atlas/icon.tres","index": 12, "amount": 10, "speed": 4, "level": 1},
-	{"name": "Staff","friendly":false,    "category": "weapon", "type": "damage","scale": "hp","weight": 20, "cost": 2, "icon":           "res://Assets/atlas/icon.tres","index": 14,   "amount": 8,  "speed": 6, "level": 1},
+	{"name": "Club","friendly":true, "category": "weapon", "type": "damage","weight": 20, "cost": 2, "icon":           "res://Assets/atlas/icon.tres","index": 12, "amount": 10, "speed": 4, "level": 1},
+	{"name": "Staff","friendly":false,    "category": "weapon", "type": "damage","weight": 20, "cost": 2, "icon":           "res://Assets/atlas/icon.tres","index": 14,   "amount": 8,  "speed": 6, "level": 1},
 	
 	# --- AUDIENCE UPGRADES ---
-	{"name": "Patron", "category": "viewer", "type": "heal",   "value": 5,"weight": 30, "throw_chance": 0.005, "cost": 3, "icon":  "res://Assets/atlas/leaf.tres","index": 5, "level": 1},
-	{"name": "Hooligan",   "category": "viewer", "type": "damage", "value": 10,"weight": 20, "throw_chance": 0.003, "cost": 3, "icon":  "res://Assets/atlas/leaf.tres","index": 7, "level": 1},
-	{"name": "Zealot", "category": "viewer", "type": "damage", "value": 50,"weight": 11, "throw_chance": 0.001, "cost": 5, "icon":  "res://Assets/atlas/leaf.tres","index": 11, "level": 1},
-	{"name": "Heckler", "category": "viewer", "type": "damage", "value": 0,"weight": 24, "throw_chance": 0.000, "cost": 5, "icon":  "res://Assets/atlas/leaf.tres","index": 15, "level": 1},
+	{"name": "Patron", "category": "viewer", "type": "heal",   "value": 5,"weight": 30, "throw_chance": 0.005, "cost": 3, "icon":  "res://Assets/atlas/fruit.tres","index": 5, "level": 1},
+	{"name": "Hooligan",   "category": "viewer", "type": "damage", "value": 10,"weight": 20, "throw_chance": 0.003, "cost": 3, "icon":  "res://Assets/atlas/fruit.tres","index": 7, "level": 1},
+	{"name": "Zealot", "category": "viewer", "type": "damage", "value": 50,"weight": 11, "throw_chance": 0.001, "cost": 5, "icon":  "res://Assets/atlas/fruit.tres","index": 11, "level": 1},
+	{"name": "Heckler", "category": "viewer", "type": "damage", "value": 0,"weight": 24, "throw_chance": 0.000, "cost": 5, "icon":  "res://Assets/atlas/fruit.tres","index": 15, "level": 1},
 ]
